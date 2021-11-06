f885a36d5ea82340a76340459e16d6588719d320
jabed37 <jabed.aleshasolutions@gmail.com>
2021-11-06 17:07:09 +0600

    basic dart & more basic about dart

---
+++ basic.dart
@@ -0,0 +1,10 @@
+void main() {
+  // print("hello world!");
+  addNumber(2.5, 3);
+  print(addNumber(5, 6));
+}
+
+double addNumber(double num1, double num2) {
+  // print(nam1 + num2);
+  return num1 + num2;
+}
---
+++ more_basic.dart
@@ -0,0 +1,20 @@
+class Person {
+  String name = "jabed";
+  int age = 30;
+}
+
+void main() {
+  // print("hello world!");
+  addNumber(2.5, 3);
+  // var firstResult = addNumber(5, 6);
+  // firstResult = addNumber(1, 2);
+  // print(firstResult + 1);
+
+  var p1 = Person();
+  print(p1.name);
+}
+
+double addNumber(double num1, double num2) {
+  // print(nam1 + num2);
+  return num1 + num2;
+}
